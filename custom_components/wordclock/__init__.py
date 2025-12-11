f53b7c718ff5a23b5b0f2c7f3f0bcd04f396c923
Andreas Uppström <uppstrom@gmail.com>
2025-12-11 23:20:58 +0100

    Corrected async_forward_entry_setups call

--- custom_components/wordclock/__init__.py
+++ custom_components/wordclock/__init__.py
@@ -38,13 +38,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(hass, entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(hass, entry, PLATFORMS):
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
